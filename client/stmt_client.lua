71a5be6243697f565c97326cd7489d922c68cfb2
Jeroen Bobbeldijk <jeroen@vdmi.nl>
2014-07-15 00:15:12 +0200

    Proper create in thread now, blip/pickup disappear now

--- client/stmt_client.lua
+++ client/stmt_client.lua
@@ -1,7 +1,8 @@
 function HospitalPickup(arg)
 	-- args is what's passed with the callback
 	TriggerEvent('chatMessage', 'Hospital', { 0, 0x99, 255 }, "You get " .. arg[1] .. " health from the hospital." )
-	SetCharHealth(GetPlayerPed(), arg[1])
+	local curHealth = GetCharHealth(GetPlayerPed());
+	SetCharHealth(GetPlayerPed(), (curHealth + arg[1]))
 end
 
 function CreateHospital(x, y, z, health)
@@ -9,4 +10,6 @@ function CreateHospital(x, y, z, health)
 	pickupHelper.createPickup(0x3675A6C3, 23, 62, x, y, z, HospitalPickup, health)
 end
 
-CreateHospital(1246.63806, 485.62134, 29.53984, 150)
+CreateThread(function()
+	CreateHospital(1246.63806, 485.62134, 29.53984, 10)
+end)
